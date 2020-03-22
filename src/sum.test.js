146fec3c6ccbbdb72962ce2780bf904d3908d12b
Daniel Pritchett ⚡ <dpritchett@gmail.com>
2020-03-22 17:46:45 -0500

    Add js comments to spike files explaining why they were added (#4)

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,3 +1,4 @@
+// this file provides our first and only test, an excuse to run jest on every build so that we hopefully always ship working code and add tests for our new features
 const sum = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
