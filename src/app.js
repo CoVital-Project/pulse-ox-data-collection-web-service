edb2c9c50662f938094f1ead063bece3b4b97bd9
Daniel J. Pritchett <dpritchett@gmail.com>
2020-03-22 17:38:03 -0500

    Add js comments to spike files explaining why they were added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
-const sum = require('./sum')
 const port = 3000
 
+// proof that we can import a locally defined module
+const sum = require('./sum')
+
 let n = sum(1,1)
+
+// proof that our imported module executes
 app.get('/', (req, res) => res.send(`Hello World! 1 + 1 = ${n}`))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,5 @@
+// this module's purpose is to prove that our main webapp can import other modules.
+// it also exists so that we can prove that our test harness works as sum.test.js executes via jest.
 function sum(a, b) {
   return a + b;
 }
