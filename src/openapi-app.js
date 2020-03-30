2aeb94705a27ab22883caba04df224c77d837be5
imonthercks <imonthercks@gmail.com>
2020-03-30 16:42:49 -0500

    reverted ssl configuration temporarily

--- src/openapi-app.js
+++ src/openapi-app.js
@@ -1,11 +1,8 @@
 import {} from 'dotenv/config'; //tricky - Improve
 import express from 'express';
 import {mongooseConnect} from './config/db';
-import {auth, requiresAuth, sslOpts} from './ssl_config';
 
 const swaggerUi = require('swagger-ui-express');
-
-const https = require('https');
 
 const swaggerDocument = require('./api_schema');
 const api = require('./api').api;
@@ -50,31 +47,18 @@ app.use(checkJwt);
 // use as express middleware
 app.use((req, res) => api.handleRequest(req, req, res));
 
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth());
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-    res.send(req.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
-
-app.get('/profile', requiresAuth(), (req, res) => {
-    res.send(JSON.stringify(req.openid.user));
-});
-
 // Tasks to run on start up
 (async () => {
     try {
-        await mongooseConnect();
-
-        console.info('[init] Successfuly connected to mongodb');
+      await mongooseConnect();
+  
+      console.info('[init] Successfuly connected to mongodb');
     } catch (err) {
-        console.info('[init] Mongodb connect error:\n', err);
-        process.exit(2);
+      console.info('[init] Mongodb connect error:\n', err);
+      process.exit(2);
     }
-})();
-
-
-// start server
-https.createServer(sslOpts, app)
-    .listen(port, () => console.log(`Covital backend listening on port ${port}...`))
+  })();
+  // start server
+  app.listen(port, () =>
+    console.log(`Covital backend listening on port ${port}...`)
+  );
