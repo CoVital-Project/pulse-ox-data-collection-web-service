6a78ce0663f2b63decdd6bb7df4b54a2659bbcda
dave <dave@baker-hagman.com>
2020-03-31 23:29:38 -0400

    Add batch signed upload request method

--- src/service/aws/s3.js
+++ src/service/aws/s3.js
@@ -1,10 +1,14 @@
+import { Promise } from "bluebird";
+
 const AWS = require('aws-sdk');
 
+
 export default class S3Service {
-  constructor(defaultVideoUploadBucket, region = 'us-east-2') {
+  constructor(defaultVideoUploadBucket, region = 'us-east-2', signedUrlTTL) {
     this._videoUploadBucket = defaultVideoUploadBucket;
     this._region = region;
     this._s3 = new AWS.S3({ region: this._region });
+    this._signedUrlTTL = signedUrlTTL;
   }
 
   listBuckets() {
@@ -16,12 +20,12 @@ export default class S3Service {
     })
   }
 
-  getSignedUploadReq(fileName, fileType) {
+  getSignedUploadReq(surveyId, fileName, fileType) {
     return new Promise((resolve, reject) => {
       const s3Params = {
         Bucket: this._videoUploadBucket,
-        Key: `${fileName}.${fileType}`,
-        Expires: 60
+        Key: this._generateFullPath(surveyId, fileName, fileType),
+        Expires: this._signedUrlTTL
       };
   
       this._s3.getSignedUrl('putObject', s3Params, (err, data) => {
@@ -29,5 +33,28 @@ export default class S3Service {
         else resolve({ signedRequest: data });
       });
     });
+  }
+
+  /**
+   * 
+   * @param [{ name: string, extension: string }] requests 
+   */
+  async batchedSignedUploadReqs(surveyId, requests) {
+    return Promise.map(requests, async req => {
+      const resp = await this.getSignedUploadReq(surveyId, req.name, req.extension);
+      return { name: req.name, extension: req.extension, key: `${req.name}.${req.extension}`, signedRequest: resp.signedRequest };
+    })
+  }
+
+  _generateFullPath(surveyId, fileName, fileExt) {
+    const now = new Date();
+    const year = now.getUTCFullYear();
+    const month = this._leftPadNumber(now.getUTCMonth() + 1); // Month is zero indexed...
+    const day = this._leftPadNumber(now.getUTCDate());
+    return `year=${year}/month=${month}/day=${day}/survey-id=${surveyId}/${fileName}.${fileExt}`;
+  }
+
+  _leftPadNumber(num, maxLength = 2, fillStr = '0') {
+    return num.toString().padStart(maxLength, '0');
   }
 }
