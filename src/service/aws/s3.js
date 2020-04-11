0345e1cbeaf4fd74d9b0a7d7a89ae786883afbae
dave <dave@baker-hagman.com>
2020-04-11 08:02:51 -0400

    Add source to signed upload API

--- src/service/aws/s3.js
+++ src/service/aws/s3.js
@@ -2,6 +2,8 @@ import { Promise } from 'bluebird';
 
 const AWS = require('aws-sdk');
 
+const ValidSources = new Set(['community', 'clinical']);
+const invalidSourcesString = Array.from(ValidSources).join(', ');
 
 export default class S3Service {
   constructor(defaultVideoUploadBucket, region = 'us-east-2', signedUrlTTL) {
@@ -20,11 +22,13 @@ export default class S3Service {
     })
   }
 
-  getSignedUploadReq(surveyId, fileName, fileType) {
+  getSignedUploadReq(surveyId, fileName, fileType, source) {
     return new Promise((resolve, reject) => {
+      if(!ValidSources.has(source)) return reject(new Error(`Invalid source. Valid values: [${invalidSourcesString}]`));
+
       const s3Params = {
         Bucket: this._videoUploadBucket,
-        Key: this._generateFullPath(surveyId, fileName, fileType),
+        Key: this._generateFullPath(surveyId, fileName, fileType, source),
         Expires: this._signedUrlTTL
       };
   
@@ -39,19 +43,19 @@ export default class S3Service {
    * 
    * @param [{ name: string, extension: string }] requests 
    */
-  async batchedSignedUploadReqs(surveyId, requests) {
+  async batchedSignedUploadReqs(surveyId, source, requests) {
     return Promise.map(requests, async req => {
-      const resp = await this.getSignedUploadReq(surveyId, req.name, req.extension);
+      const resp = await this.getSignedUploadReq(surveyId, req.name, req.extension, source);
       return { name: req.name, extension: req.extension, key: `${req.name}.${req.extension}`, signedRequest: resp.signedRequest };
     })
   }
 
-  _generateFullPath(surveyId, fileName, fileExt) {
+  _generateFullPath(surveyId, fileName, fileExt, source) {
     const now = new Date();
     const year = now.getUTCFullYear();
     const month = this._leftPadNumber(now.getUTCMonth() + 1); // Month is zero indexed...
     const day = this._leftPadNumber(now.getUTCDate());
-    return `year=${year}/month=${month}/day=${day}/survey-id=${surveyId}/${fileName}.${fileExt}`;
+    return `source=${source}/year=${year}/month=${month}/day=${day}/survey-id=${surveyId}/${fileName}.${fileExt}`;
   }
 
   _leftPadNumber(num, maxLength = 2, fillStr = '0') {
